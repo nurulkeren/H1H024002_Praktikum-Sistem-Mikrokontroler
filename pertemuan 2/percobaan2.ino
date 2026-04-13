345831b47d8eeb4661bef452a1a4b1b45addde73
nurulkeren <maftuhahnurulgacor@gmail.com>
2026-04-13 22:37:39 +0700

    Update percobaan2.ino

--- pertemuan 2/percobaan2.ino
+++ pertemuan 2/percobaan2.ino
@@ -1,9 +1,7 @@
-#include <Arduino.h>
+// Pin mapping segment (a b c d e f g dp)
+const int segmentPins[8] = {7, 6, 5, 10, 11, 8, 9, 4};
 
-// Pin 7-Segment (a b c d e f g dp)
-const int segmentPins[8] = {7, 6, 5, 11, 10, 8, 9, 4};
-
-// Push button
+// Button
 const int buttonPin = 2;
 
 // Counter
@@ -12,39 +10,33 @@ int counter = 0;
 // State button
 bool lastButtonState = HIGH;
 
-// Pola digit 0-F
 byte digitPattern[16][8] = {
-  {1,1,1,1,1,1,0,0}, //0
-  {0,1,1,0,0,0,0,0}, //1
-  {1,1,0,1,1,0,1,0}, //2
-  {1,1,1,1,0,0,1,0}, //3
-  {0,1,1,0,0,1,1,0}, //4
-  {1,0,1,1,0,1,1,0}, //5 
-  {1,0,1,1,1,1,1,0}, //6
-  {1,1,1,0,0,0,0,0}, //7
-  {1,1,1,1,1,1,1,0}, //8
-  {1,1,1,1,0,1,1,0}, //9
-  {1,1,1,0,1,1,1,0}, //A
-  {0,0,1,1,1,1,1,0}, //b
-  {1,0,0,1,1,1,0,0}, //C
-  {0,1,1,1,1,0,1,0}, //d
-  {1,0,0,1,1,1,1,0}, //E
-  {1,0,0,0,1,1,1,0}  //F
+  {1,1,1,1,1,1,0,0}, // 0
+  {0,1,1,0,0,0,0,0}, // 1
+  {1,1,0,1,1,0,1,0}, // 2
+  {1,1,1,1,0,0,1,0}, // 3
+  {0,1,1,0,0,1,1,0}, // 4
+  {1,0,1,1,0,1,1,0}, // 5
+  {1,0,1,1,1,1,1,0}, // 6
+  {1,1,1,0,0,0,0,0}, // 7
+  {1,1,1,1,1,1,1,0}, // 8
+  {1,1,1,1,0,1,1,0}, // 9
+  {1,1,1,0,1,1,1,0}, // A
+  {0,0,1,1,1,1,1,0}, // b
+  {1,0,0,1,1,1,0,0}, // C
+  {0,1,1,1,1,0,1,0}, // d
+  {1,0,0,1,1,1,1,0}, // E
+  {1,0,0,0,1,1,1,0}  // F
 };
 
-// Tampilkan digit
-void displayDigit(int num)
-{
-  for(int i=0; i<8; i++)
-  {
-    digitalWrite(segmentPins[i], !digitPattern[num][i]);
+void displayDigit(int num) {
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(segmentPins[i], !digitPattern[num][i]); // Common Anode
   }
 }
 
-void setup()
-{
-  for(int i=0; i<8; i++)
-  {
+void setup() {
+  for (int i = 0; i < 8; i++) {
     pinMode(segmentPins[i], OUTPUT);
   }
 
@@ -53,17 +45,15 @@ void setup()
   displayDigit(counter); // tampilkan awal
 }
 
-void loop()
-{
+void loop() {
   bool currentButtonState = digitalRead(buttonPin);
 
-  // deteksi tekan (HIGH -> LOW)
-  if (lastButtonState == HIGH && currentButtonState == LOW)
-  {
+  // Deteksi tekan (HIGH → LOW)
+  if (lastButtonState == HIGH && currentButtonState == LOW) {
     counter++;
-    if(counter > 15) counter = 0;
+    if (counter > 15) counter = 0;
 
-    displayDigit(counter); // update hanya saat ditekan
+    displayDigit(counter);
 
     delay(200); // debounce sederhana
   }
